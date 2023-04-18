ee9f403d0ddcccd80331cb7b31f301120e38b974
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-04-18 15:45:55 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,63 +9,28 @@ using namespace std;
 
 class Solution{   
 public:
-
-    // bool solve(vector<int> &arr,int sum,int n,vector<vector<int>> & dp){
-    //     if(n==0){
-    //         if(sum==0)
-    //         return true;
-    //         else
-    //         return false;
-    //     }
-        
-    //     if(sum==0)
-    //     return true;
-        
-    //     if(dp[n][sum]!=-1)
-    //     return dp[n][sum];
-        
-    //     bool left,right=false;
-    //     if(arr[n-1]<=sum)
-    //     left=solve(arr,sum-arr[n-1],n-1,dp) or solve(arr,sum,n-1,dp);
-    //     else
-    //   right= dp[n][sum]=solve(arr,sum,n-1,dp);
-        
-    //     return dp[n][sum]=left or right;
-        
-    // }
-    // bool isSubsetSum(vector<int>arr, int sum){
-    //     int n=arr.size();
-    //     vector<vector<int>> dp(n+1,vector<int>(sum+1),-1);
-    //     return solve(arr,sum,n-1,dp);
-    // }
-    
-    bool solve(vector<int> &arr,int sum,int n,vector<vector<int>> & dp){
-        if(n==0){
-            if(sum==0)
-            return true;
-            else
-            return false;
-        }
-        
-        if(sum==0)
-        return true;
-        
-        if(dp[n][sum]!=-1)
-        return dp[n][sum];
-        
-        bool left,right=false;
-        if(arr[n-1]<=sum)
-        left=solve(arr,sum-arr[n-1],n-1,dp) or solve(arr,sum,n-1,dp);
-        else
-       right= dp[n][sum]=solve(arr,sum,n-1,dp);
-        
-        return dp[n][sum]=left or right;
-        
-    }
     bool isSubsetSum(vector<int>arr, int sum){
         int n=arr.size();
-        vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
-        return solve(arr,sum,n,dp);
+        vector<vector<bool>> dp(n+1,vector<bool>(sum+1,-1));
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(i==0)
+                dp[i][j]=false;
+                if(j==0)
+                dp[i][j]=true;
+            }
+        }
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(arr[i-1]<=j)
+                dp[i][j]=dp[i-1][j-arr[i-1]] or dp[i-1][j];
+                else
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+        
+        return dp[n][sum];
     }
 };
 
