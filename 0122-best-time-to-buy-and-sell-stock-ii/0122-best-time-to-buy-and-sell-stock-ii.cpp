f493a34c0f49312a406f4f69da9404e27d468e3e
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-04-21 09:43:31 +0530

    Time: 19 ms (8.11%), Space: 15.2 MB (23.97%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     
-    int solve(vector<int> &prices, int i,int buy, vector<vector<int>> & dp){
+   /* 
+   //this is top down approach
+   int solve(vector<int> &prices, int i,int buy, vector<vector<int>> & dp){
         if(i==prices.size())
             return 0;
         
@@ -20,11 +22,31 @@ public:
         
         return dp[i][buy]=profit;
     }
+    */
+    
+    
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         
-        vector<vector<int>> dp(n,vector<int>(2,-1));
+        vector<vector<int>> dp(n+1,vector<int>(2,-1));
         
-        return solve(prices,0,0,dp);
+       // return solve(prices,0,0,dp);
+        
+        dp[n][0]=dp[n][1]=0;
+        int buy=0;
+        int profit;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                if(!j){
+                    profit =max(-prices[i]+dp[i+1][1],dp[i+1][0]);   
+                }
+                else
+                    profit=max(prices[i]+dp[i+1][0],dp[i+1][1]);
+                
+                dp[i][j]=profit;
+            }
+        }
+        
+        return dp[0][0];
     }
 };
