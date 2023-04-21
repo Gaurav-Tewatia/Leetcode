5d785633cc319f1801a83112f429d38a3ab54a8e
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-04-21 16:47:06 +0530

    Time: 450 ms (35.15%), Space: 10.8 MB (35.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -18,23 +18,25 @@ public:
 //     }
     
     
-    int solvebottomup(vector<int> &nums, vector<vector<int>> &dp,int n){
+    
+//     //this is the code for bottom up approach
+//     int solvebottomup(vector<int> &nums, vector<vector<int>> &dp,int n){
         
-        for(int cur=n-1;cur>=0;cur--){
-            for(int prev=cur-1;prev>=-1;prev--){
-                int take=0;
-                if(prev==-1 or nums[cur]>nums[prev])
-                    take=1+dp[cur+1][cur+1];
+//         for(int cur=n-1;cur>=0;cur--){
+//             for(int prev=cur-1;prev>=-1;prev--){
+//                 int take=0;
+//                 if(prev==-1 or nums[cur]>nums[prev]) //here we have not donw prev+1 as for prev -1 we are already checking it
+//                     take=1+dp[cur+1][cur+1]; //this is done because -1 index is not present in the vector
                 
-                int nottake=dp[cur+1][prev+1];
+//                 int nottake=dp[cur+1][prev+1];
                 
-                dp[cur][prev+1]=max(take,nottake);
-            }
-        }
+//                 dp[cur][prev+1]=max(take,nottake);
+//             }
+//         }
         
-        return dp[0][0];
+//         return dp[0][0];
         
-    }
+//     }
     
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
@@ -42,8 +44,25 @@ public:
         
        // return solvemem(nums,0,-1,dp,n);
         
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+//         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
         
-        return solvebottomup(nums,dp,n);
+//         return solvebottomup(nums,dp,n);
+        
+        vector<int> cur(n+1,0),prev(n+1,0);
+        
+        for(int currow=n-1;currow>=0;currow--){
+            for(int prevrow=currow-1;prevrow>=-1;prevrow--){
+                int take=0;;
+                if(prevrow==-1 or nums[currow]>nums[prevrow])
+                    take=1+prev[currow+1];
+                
+                int nottake=prev[prevrow+1];
+                
+                cur[prevrow+1]=max(take, nottake);
+            }
+            prev=cur;
+        }
+        
+        return prev[0];
     }
 };
