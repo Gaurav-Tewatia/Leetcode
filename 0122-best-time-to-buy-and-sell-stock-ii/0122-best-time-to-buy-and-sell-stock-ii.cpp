7ada604cf8fd7d959481f85c8e84d5e32c6286d4
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-04-21 10:24:03 +0530

    Time: 15 ms (19.04%), Space: 15.1 MB (28.51%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -32,7 +32,7 @@ public:
         
        // return solve(prices,0,0,dp);
         
-        dp[n][0]=dp[n][1]=0;
+      /*  dp[n][0]=dp[n][1]=0;
         int buy=0;
         int profit;
         for(int i=n-1;i>=0;i--){
@@ -46,7 +46,27 @@ public:
                 dp[i][j]=profit;
             }
         }
+        */
         
-        return dp[0][0];
+        //this is space optimized version
+        
+        vector<int> ahead(2,0),cur(2,0);
+        ahead[0]=ahead[1]=0;
+         for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                
+                int profit=0;
+                if(!j){
+                    profit =max(-prices[i]+ahead[1],ahead[0]);   
+                }
+                else
+                    profit=max(prices[i]+ahead[0],ahead[1]);
+                
+                cur[j]=profit;
+            }
+             ahead=cur;
+        }
+        
+        return ahead[0];
     }
 };
