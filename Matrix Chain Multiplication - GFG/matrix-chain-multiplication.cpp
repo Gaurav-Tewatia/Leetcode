993c3bf3b81ac5b4532aaa7b9b921d3ff76813af
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-04-22 20:20:10 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -12,27 +12,46 @@ class Solution{
     
 public:
 
-
-    int solve(int i, int j, int *arr,vector<vector<int>> &dp){
-        if(i==j)
-        return 0;
+    // static vector<vector<int>> dp(100,vector<int>(100,-1)); 
+    // int solve(int i, int j, int *arr){
+    //     if(i==j)
+    //     return 0;
         
-        if(dp[i][j]!=-1)
-        return dp[i][j];
+    //     if(dp[i][j]!=-1)
+    //     return dp[i][j];
         
-        int mini=INT_MAX;
-        for(int k=i;k<j;k++){
-            int steps=arr[i-1]*arr[k]*arr[j]+solve(i,k,arr,dp)+solve(k+1,j,arr,dp);
-            mini=min(steps,mini);
-        }
+    //     int mini=INT_MAX;
+    //     for(int k=i;k<j;k++){
+    //         int steps=arr[i-1]*arr[k]*arr[j]+solve(i,k,arr)+solve(k+1,j,arr);
+    //         mini=min(steps,mini);
+    //     }
         
-        return dp[i][j]=mini;
-    }
+    //     return dp[i][j]=mini;
+    // }
     
     int matrixMultiplication(int N, int arr[])
     {
                
-        return solve(1,N-1,arr,dp);
+       // return solve(1,N-1,arr);
+        
+        
+        int dp[N][N];
+        for(int i=1;i<N;i++)
+        dp[i][i]=0;
+        
+        for(int i=N-1;i>=1;i--){
+            for(int j=i+1;j<N;j++){
+                int mini=INT_MAX;
+                for(int k=i;k<j;k++){
+                    int steps=arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+                    
+                    mini=min(mini,steps);
+                }
+                dp[i][j]=mini;
+            }
+            
+        }
+        return dp[1][N-1];
     }
 };
 
