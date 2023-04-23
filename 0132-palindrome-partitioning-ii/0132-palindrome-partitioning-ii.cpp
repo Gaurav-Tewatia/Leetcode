9ac07e6c3d2d61191f6608c9ea93eca594716d01
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-04-23 11:55:20 +0530

    Time: 390 ms (49.54%), Space: 6.5 MB (75.51%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -9,29 +9,50 @@ public:
         }
         return true;
     }
-    int f(int i, int n ,string &s,vector<int> &dp){
-        if(i==n)
-            return 0;
+//     int f(int i, int n ,string &s,vector<int> &dp){
+//         if(i==n)
+//             return 0;
         
-        if(dp[i]!=-1)
-            return dp[i];
+//         if(dp[i]!=-1)
+//             return dp[i];
         
-        int mini=INT_MAX;
+//         int mini=INT_MAX;
+        
+//         for(int k=i;k<n;k++){
+//             int partition=0;
+            
+//             if(ispossible(i,k,s)){
+//                 int ans= 1+f(k+1,n,s,dp);
+//                 mini=min(mini,ans);
+//             }
+//         }
+//         return dp[i]=mini;
+        
+//     }
+    int minCut(string s) {
+        int n=s.size();
+        // vector<int> dp(n,-1);
+        // return f(0,n,s,dp)-1;
+        
+        vector<int> dp(n+1,0);
+        // dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            
+             int mini=INT_MAX;
         
         for(int k=i;k<n;k++){
             int partition=0;
             
             if(ispossible(i,k,s)){
-                int ans= 1+f(k+1,n,s,dp);
+                int ans= 1+dp[k+1];
                 mini=min(mini,ans);
             }
         }
-        return dp[i]=mini;
         
-    }
-    int minCut(string s) {
-        int n=s.size();
-        vector<int> dp(n,-1);
-        return f(0,n,s,dp)-1;
+            dp[i]=mini;
+            
+        }
+        
+        return dp[0]-1;
     }
 };
