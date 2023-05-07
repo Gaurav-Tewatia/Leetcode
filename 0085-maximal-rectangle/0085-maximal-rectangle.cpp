580858dcdb4a77f7db78cc7e0b1cfdaa76f5761a
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-05-07 15:32:24 +0530

    Time: 72 ms (21.01%), Space: 22.5 MB (7.31%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,40 +1,85 @@
 class Solution {
 public:
-     int largestRectangleArea(vector < int > & histo) {
-      stack < int > st;
-      int maxA = 0;
-      int n = histo.size();
-      for (int i = 0; i <= n; i++) {
-        while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
-          int height = histo[st.top()];
-          st.pop();
-          int width;
-          if (st.empty())
-            width = i;
-          else
-            width = i - st.top() - 1;
-          maxA = max(maxA, width * height);
-        }
-        st.push(i);
-      }
-      return maxA;
-    }
-    
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
+     int histogram(vector<int>& v){
+       stack<pair<int,int>> st;
+        vector<long long> nsl, nsr;
         
-        vector<int> height(m,0);
-        int maxi=INT_MIN;
+        int n=v.size();
+        
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                
-                if(matrix[i][j]=='1') height[j]++;
+            if(st.empty())
+                nsl.push_back(-1);
+            else if(!st.empty() and st.top().first<v[i])
+                nsl.push_back(st.top().second);
+            else if(!st.empty() and st.top().first>=v[i]){
+                while(!st.empty() and st.top().first>=v[i])
+                    st.pop();
+                if(st.empty())
+                    nsl.push_back(-1);
                 else
-                    height[j]=0;
+                    nsl.push_back(st.top().second);
             }
-            int temp=largestRectangleArea(height);
-            maxi=max(temp,maxi);
+            st.push({v[i],i});
+        }
+        stack<pair<int,int>> temp;
+        st.swap(temp);
+    
+         for(int i=n-1;i>=0;i--){
+            if(st.empty())
+                nsr.push_back(n);
+            else if(!st.empty() and st.top().first<v[i])
+                nsr.push_back(st.top().second);
+            else if(!st.empty() and st.top().first>=v[i]){
+                while(!st.empty() and st.top().first>=v[i])
+                    st.pop();
+                if(st.empty())
+                    nsr.push_back(n);
+                else
+                    nsr.push_back(st.top().second);
+            }
+            st.push({v[i],i});
+        }
+        
+        reverse(nsr.begin(),nsr.end());
+    int ans=0;
+         for(int i=0;i<n;i++){
+             int width=v[i]*(nsr[i]-nsl[i]-1);
+             
+             ans=max(width,ans);
+         }
+         
+         // for(auto c: nsl)
+         //     cout<<c<<" ";
+         // cout<<endl;
+         // for(auto c: nsr)
+         //     cout<<c<<" ";
+         // cout<<endl;
+         
+                     return ans;
+    
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+         int n=matrix[0].size();
+        vector<int> v(n,0);
+        for(int i=0;i<n;i++)
+            {
+                v[i]=matrix[0][i]-'0';
+            }
+        
+        for(auto c: v)
+            cout<<c<<" ";
+
+        
+        int maxi=histogram(v);
+        for(int i=1;i<matrix.size();i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]=='1')
+                    v[j]+=1;
+                else 
+                    v[j]=0;
+                
+            }
+            maxi=max(histogram(v),maxi);
         }
         
         return maxi;
