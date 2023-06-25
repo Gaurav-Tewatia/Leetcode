313e1f4d45f88388c04f0caff1b8217a75abc7be
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-06-25 15:58:27 +0530

    Added solution - LeetHub

---
+++ Articulation Point - I - GFG/articulation-point-i.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+    private:
+    int timer=0;
+        void dfs(int node,int parent,int *tin, int *low, vector<int>&vis, vector<int> adj[]
+        ,vector<int>&vertex){
+            vis[node]=1;
+            tin[node]=low[node]=timer;
+            timer++;
+            int child=0;
+            for(auto it:adj[node]){
+                if(it==parent) continue;
+                if(!vis[it]){
+                    dfs(it,node,tin,low,vis,adj,vertex);
+                    low[node]=min(low[it],low[node]);
+                    if(low[it]>=tin[node] and parent!=-1)
+                    vertex[node]=1;
+                    
+                    child++;
+                }
+                else
+                low[node]=min(tin[it],low[node]);
+                
+            }
+            if(child>1 and parent==-1)
+            vertex[node]=1;
+        }
+  public:
+    vector<int> articulationPoints(int V, vector<int>adj[]) {
+        int tin[V];
+        int low[V];
+        vector<int> vis(V,0);
+        vector<int> vertex(V,0);
+        for(int i=0;i<V;i++){
+            if(!vis[i])
+            dfs(i,-1,tin,low,vis,adj,vertex);
+        }
+        
+        vector<int> ans;
+        for(int i=0;i<V;i++){
+            if(vertex[i]==1)
+            ans.push_back(i);
+        }
+        if(ans.size()==0) return {-1};
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		vector<int> ans = obj.articulationPoints(V, adj);
+		for(auto i: ans)cout << i << " ";
+		cout << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
