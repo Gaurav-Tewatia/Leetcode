1c0fe32315aae9c11ff582ae14fa8a1e7a9bac3a
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-06-28 18:14:26 +0530

    Added solution - LeetHub

--- Capacity To Ship Packages Within D Days - GFG/capacity-to-ship-packages-within-d-days.cpp
+++ Capacity To Ship Packages Within D Days - GFG/capacity-to-ship-packages-within-d-days.cpp
@@ -26,9 +26,12 @@ class Solution {
     int leastWeightCapacity(int arr[], int N, int D) {
         int low=*max_element(arr,arr+N);
         int high=0;
-        for(int i=0;i<N;i++){
-            high+=arr[i];
-        }
+        // for(auto it:arr)
+        //     high+=it;
+        //this line won't work as in c++ version 5.4 range for loop does not support arrays
+        
+        for(int i=0;i<N;i++)
+        high+=arr[i];
         
         while(low<=high){
             int mid=low+(high-low)/2;
