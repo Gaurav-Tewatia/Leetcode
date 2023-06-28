5b5da53d77302a36f2670818ed4ca4dc3d0f2f5e
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-06-28 12:54:04 +0530

    Time: 221 ms (49.75%), Space: 64.9 MB (86.47%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,39 +1,45 @@
 class Solution {
 public:
-    typedef double var;
-    int n;
-    var solve(int start,int end,vector<pair<int,var>> adj[])
-    {
-        // modified dijkstra
-        vector<var> ans(n,0);
-        priority_queue<pair<var,int>> pq;
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        
+        vector<pair<int,double>> adj[n+1];
+        
+       for(int i=0;i<edges.size();i++){
+           adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
+           adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
+       }
+
+        // //for checking purpose
+        // for(int i=0;i<n;i++){
+        //     for(auto it:adj[i])
+        //         cout<<i<<"->"<<it.first<<"["<<it.second<<"]"<<" ";
+        // }
+        // cout<<endl;
+        
+    priority_queue<pair<double,int>> pq;
+        vector<double> prob(n,0);
+        prob[start]=1;
         pq.push({1,start});
-        ans[start]=0;
-        while(!pq.empty())
-        {
-            auto p=pq.top();
+        
+        while(!pq.empty()){
+            double probb=pq.top().first;
+            int node=pq.top().second;
             pq.pop();
-            auto node=p.second;
-            auto cost=p.first;
-            for(auto it:adj[node])
-            {
-                if((cost*it.second)>ans[it.first])
-                {
-                    ans[it.first]=cost*it.second;
-                    pq.push({ans[it.first],it.first});
+            
+            for(auto it:adj[node]){
+                int adjnode=it.first;
+                double wt=it.second;
+                
+                if(double(probb*wt)>prob[adjnode]){
+                    prob[adjnode]=(double)(probb*wt);
+                    pq.push({prob[adjnode],adjnode});
                 }
             }
         }
-        return ans[end];
-    }
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<pair<int,var>> adj[n+1];
-        this->n=n;
-        for(int i=0;i<edges.size();i++)
-        {
-            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
-            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
-        }
-        return solve(start,end,adj);
+        
+        // for(auto c:prob)
+        //     cout<<c<<" ";
+        // cout<<endl;
+        return prob[end];
     }
 };
