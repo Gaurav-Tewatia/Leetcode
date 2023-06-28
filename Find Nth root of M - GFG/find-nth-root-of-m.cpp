0373e7960e68d8922e6e6161e8502de48d4e0d43
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-06-28 15:01:05 +0530

    Added solution - LeetHub

--- Find Nth root of M - GFG/find-nth-root-of-m.cpp
+++ Find Nth root of M - GFG/find-nth-root-of-m.cpp
@@ -10,7 +10,7 @@ class Solution{
 	    int low=1,high=m;
 	    
 	    while(low<=high){
-	        long long mid=low+(high-low)/2;
+	        int mid=low+(high-low)/2;
 	        if(pow(mid,n)==m)
 	        return mid;
 	        else if(pow(mid,n)<m)
