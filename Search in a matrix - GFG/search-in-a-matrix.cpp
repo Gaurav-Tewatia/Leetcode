b29520e5664a0533a29159878571ac4dae921077
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-06-30 12:37:40 +0530

    Added solution - LeetHub

--- Search in a matrix - GFG/search-in-a-matrix.cpp
+++ Search in a matrix - GFG/search-in-a-matrix.cpp
@@ -5,21 +5,39 @@ using namespace std;
 
 // } Driver Code Ends
 //User function template for C++
+
+//User function template for C++
+//User function template for C++
+
+//User function template for C++
 class Solution{
+    private:
+     int find(vector<int> &mat,int low,int high,int x){
+         
+         while(low<=high){
+             int mid=low+(high-low)/2;
+             if(mat[mid]==x) return mid;
+             else if(mat[mid]>x)
+             high=mid-1;
+             else
+             low=mid+1;
+         }
+         
+         return -1;
+     }
 public:	
 	int matSearch (vector <vector <int>> &mat, int N, int M, int X)
 	{
-	    int row=0, col=M-1;
-	    while(row<N and col>=0){
-	        if(mat[row][col]==X) return 1;
-	        else if(mat[row][col]<X)
-	        row++;
-	        else
-	        col--;
-	    }
-	    return 0;
+	   for(int i=0;i<N;i++){
+	       
+	       int col=find(mat[i],0,M-1,X);
+	       if(col!=-1)
+	       return 1;
+	   }
+	   return 0;
 	}
 };
+
 
 //{ Driver Code Starts.
 
