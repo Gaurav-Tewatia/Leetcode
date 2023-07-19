dd4c8e14cbffa83717554579a290b90f0b5a3c91
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-07-19 15:39:53 +0530

    Time: 7 ms (81.78%), Space: 9.4 MB (54.56%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,12 +1,27 @@
 class Solution {
+    private:
+    void helper(int i, int n, vector<int> nums,vector<int> &v,vector<vector<int> > &ans){
+        if(i==n){
+            ans.push_back(v);
+            return;
+        }
+        
+        for(int idx=i;idx<n;idx++){
+            if(idx!=i and (nums[idx]==nums[idx-1] or nums[idx]==nums[i])) continue;
+            swap(nums[idx],nums[i]);
+            v.push_back(nums[i]);
+            helper(i+1,n,nums,v,ans);
+           // swap(nums[idx],nums[i]);
+            v.pop_back();
+        }
+    }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        ans.push_back(nums);
-        while(next_permutation(nums.begin(),nums.end())){
-            ans.push_back(nums);
-        }
+        vector<int> v;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        helper(0,n,nums,v,ans);
         return ans;
     }
 };
