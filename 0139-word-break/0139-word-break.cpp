1124bc6131f03ada451a26ed838e5435dc5964db
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-07-20 17:30:48 +0530

    Time: 9 ms (63.73%), Space: 8.2 MB (75.40%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    bool found(string &path,vector<string> &wordDict,map<string,int> &mp){
+        
+        if(mp.find(path)!=mp.end())
+            return true;
+        else
+            return false;
+        
+    }
+    bool solve(vector<string> &wordDict,string &s, int index,map<string,int>& mp,vector<int> &dp){
+        
+        if(index==s.size())
+            return dp[index]=true;
+        string path="";
+        if(dp[index]!=-1)
+        return dp[index];
+
+        for(int i=index;i<s.size();i++){
+            path.push_back(s[i]);
+            if(found(path,wordDict,mp)){
+                if(solve(wordDict,s,i+1,mp,dp))
+                    return dp[i]=true;
+            }
+        }
+        return dp[index]=false;
+        
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        map<string,int> mp;
+        for(auto c:wordDict){
+            mp[c]++;
+        }
+        int n =s.size();
+
+        vector<int> dp(n+1,-1);
+        
+        return solve(wordDict,s,0,mp,dp);
+    }
+};
