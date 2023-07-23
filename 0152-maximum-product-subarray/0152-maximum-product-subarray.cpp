acc3d70e9120bd1865803e4a9d26fce65f447013
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-07-23 11:36:25 +0530

    Time: 3 ms (97.21%), Space: 13.8 MB (61.06%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -7,6 +7,7 @@ public:
         for(int i=0;i<n;i++){
             if(prefix==0) prefix=1;
             if(suffix==0) suffix=1;
+            
             prefix*=nums[i];
             suffix*=nums[n-i-1];
             
