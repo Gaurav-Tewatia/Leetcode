aa13a617f0f941350cff0e460a138e654a22e455
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-07-28 10:45:03 +0530

    Added solution - LeetHub

--- Word Search - GFG/word-search.cpp
+++ Word Search - GFG/word-search.cpp
@@ -9,21 +9,21 @@ class Solution {
         return false;
     }
     bool helper(int index,int row, int col,int n ,int m, vector<vector<int>> &vis,string &word,vector<vector<char>> &board){
-        if(index==word.size()) return true;
-        
+        if(index==word.size()-1) return true;
+        vis[row][col]=1;
         int dr[]={-1,0,1,0,-1};
         
         for(int i=0;i<4;i++){
             int nrow=row+dr[i];
             int ncol=col+dr[i+1];
             
-            if(check(index,nrow,ncol,n,m,board,word) and vis[nrow][ncol]==0){
-                vis[nrow][ncol]=1;
+            if(check(index+1,nrow,ncol,n,m,board,word) and vis[nrow][ncol]==0){
+                
                 if(helper(index+1,nrow,ncol,n,m,vis,word,board)) return true;
-                vis[nrow][ncol]=0;
+                
             }
         }
-        
+        vis[row][col]=0;
         return false;
     }
 public:
@@ -35,9 +35,9 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]==word[0]){
-                    vis[i][j]=1;
-                    if(helper(1,i,j,n,m,vis,word,board)) return true;
-                    vis[i][j]=0;
+                    
+                    if(helper(0,i,j,n,m,vis,word,board)) return true;
+                    
                 }
             }
         }
