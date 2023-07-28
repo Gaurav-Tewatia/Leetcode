e5b08c99ba31b8d81fa8bc3c91a5a25dcfe6edd3
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-07-28 15:32:23 +0530

    Time: 1454 ms (57.10%), Space: 6.5 MB (75.29%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,60 +1,29 @@
 class Solution {
-public:
-    bool ispossible(int i, int j, string &s){
+    bool check(int i,int j,string &s){
         while(i<j){
-            if(s[i]!=s[j])
-                return false;
-            i++;
-            j--;
+            if(s[i++]!=s[j--]) return false;
         }
         return true;
     }
-//     int f(int i, int n ,string &s,vector<int> &dp){
-//         if(i==n)
-//             return 0;
-        
-//         if(dp[i]!=-1)
-//             return dp[i];
-        
-//         int mini=INT_MAX;
-        
-//         for(int k=i;k<n;k++){
-//             int partition=0;
-            
-//             if(ispossible(i,k,s)){
-//                 int ans= 1+f(k+1,n,s,dp);
-//                 mini=min(mini,ans);
-//             }
-//         }
-//         return dp[i]=mini;
-        
-//     }
-    int minCut(string s) {
-        int n=s.size();
-        // vector<int> dp(n,-1);
-        // return f(0,n,s,dp)-1;
-        
-        
-        //bottom up
-        vector<int> dp(n+1,0);
-        
-        for(int i=n-1;i>=0;i--){
-            
-             int mini=INT_MAX;
-        
-        for(int k=i;k<n;k++){
-            int partition=0;
-            
-            if(ispossible(i,k,s)){
-                int ans= 1+dp[k+1];
-                mini=min(mini,ans);
+    int helper(int i,int n, string &s,vector<int> &dp){
+        if(i==n or check(i,n-1,s)){
+            return dp[i]=0;
+        }
+        if(dp[i]!=-1) return dp[i];
+        int cnt=INT_MAX;
+        for(int j=i;j<n;j++){
+            if(check(i,j,s)){
+                cnt=min(helper(j+1,n,s,dp)+1,cnt);
             }
         }
+        return dp[i]=cnt;
+    }
+public:
+    int minCut(string s) {
+        if(s.size()==0 or check(0,s.size()-1,s)) return 0; 
         
-            dp[i]=mini;
-            
-        }
-        
-        return dp[0]-1;
+        int n=s.size();
+        vector<int> dp(n+1,-1);
+        return helper(0,n,s,dp);
     }
 };
