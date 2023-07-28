e6ebdfc7919cdd2135827acbc07e0fdd799ebb83
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-07-28 13:30:49 +0530

    Time: 15 ms (51.59%), Space: 13.3 MB (36.63%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,40 +1,24 @@
 class Solution {
-public:
-    
-    bool found(string &path,vector<string> &wordDict,unordered_set<string> &mp){
+    bool helper(int i,int n,string &s,vector<string> &wordDict,unordered_set<string> &st,vector<int> &dp){
+        if(i==n) return dp[i]=true;
         
-       return mp.count(path);
+        if(dp[i]!=-1) return dp[i];
         
-    }
-    bool solve(vector<string> &wordDict,string &s, int index,unordered_set<string>& mp,vector<int> &dp){
-        
-        if(index==s.size())
-            return dp[index]=true;
-        string path="";
-        if(dp[index]!=-1)
-        return dp[index];
-
-        for(int i=index;i<s.size();i++){
-            path.push_back(s[i]);
-            if(found(path,wordDict,mp)){
-                if(solve(wordDict,s,i+1,mp,dp))
-                    return dp[i]=true;
+        for(int j=i;j<n;j++){
+            string substring=s.substr(i,j-i+1);
+            if(st.count(substring)){
+                if(helper(j+1,n,s,wordDict,st,dp)) return dp[j]=true;
             }
         }
-        return dp[index]=false;
-        
+        return dp[i]=false;
     }
-    
+public:
     bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> st;
+        for(auto it:wordDict) st.insert(it);
         
-        unordered_set<string> mp;
-        for(auto c:wordDict){
-            mp.insert(c);
-        }
         int n=s.size();
-
         vector<int> dp(n+1,-1);
-        
-        return solve(wordDict,s,0,mp,dp);
+        return helper(0,n,s,wordDict,st,dp);
     }
 };
