b85f234a78bf54d23e4cd417e3829e0aef03ac63
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-07-29 12:22:53 +0530

    Time: 9 ms (48.53%), Space: 7.9 MB (91.14%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
     int calculate(string s) {
-        int sign=1;
+        int n=s.size();
         int sum=0;
         stack<int> st;
-        
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if(isdigit(s[i])){
-                int num=0;
-                while(i<n and isdigit(s[i])) num=num*10+(s[i++]-'0');
-                i--;
-                num=sign*num;
-                sum+=num;
+        int sign=1 ;//1 for '+'  & -1 for '-'
+            
+            for(int i=0;i<n;i++){
+                
+                //if digit
+                if(isdigit(s[i])){
+                    int num=0;
+                    // it could be 9 or 9999  so find the entire number 
+                    
+                    while(i<n and isdigit(s[i])) num=num*10+(s[i++]-'0');
+                    i--; // it would be at an operator so do -- do go back at last digit of the number
+                    num=sign*num;
+                    sum+=num;
+                }
+                //if it is '+'
+                else if(s[i]=='+') sign=1;
+                else if(s[i]=='-') sign=-1;   // if it is'-'
+                else if(s[i]=='(')   //insert all the calculation till now in the stack
+                {
+                    st.push(sum);
+                    st.push(sign);
+                    sum=0;
+                    sign=1;
+                }
+                else if(s[i]==')')  //add the current sum to that of calculation done outside () that is inside stack
+                {
+                    sum=st.top()*sum;
+                    st.pop();
+                    sum=st.top()+sum;
+                    st.pop();
+                }
+                   
+                
             }
-            else if(s[i]=='+'){
-                sign=1;
-            }
-            else if(s[i]=='-') sign=-1;
-            else if(s[i]=='('){
-                st.push(sum);
-                st.push(sign);
-                sum=0;
-                sign=1;
-            }
-            else if(s[i]==')'){
-                sum=st.top()*sum;
-                st.pop();
-                sum=st.top()+sum;
-                st.pop();
-            }
-        }
         return sum;
     }
 };
