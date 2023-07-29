ae45639f08f18f0d5c3c9f4a82ceea48eaba44f0
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-07-29 09:49:11 +0530

    Time: 449 ms (88.44%), Space: 115.3 MB (51.54%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,35 +1,33 @@
 class Solution {
-    private:
-    void helper(int i,int n,string &num,string s,vector<string> &ans,long long cursum,int prevnum,int target){
+    typedef long long ll;
+    void helper(int i,int n,string &num,int target,ll cur,ll prev,string s,vector<string> &ans){
         if(i==n){
-            if(cursum==target) ans.push_back(s);
-            
+            if(cur==target)
+            ans.push_back(s);
             return;
         }
         
         for(int j=i;j<n;j++){
+            if(j>i and num[i]=='0') return;
             
-            //checking for leading zeros
-            
-            if(j>i and num[i]=='0') return;    //"1203" -> 12+03. here 0 is leading
-            
-            string str=num.substr(i,j-i+1);
-            long long number=stol(str);
+            string temp=num.substr(i,j-i+1);
+            long long digit=stol(temp);
             if(i==0){
-                helper(j+1,n,num,s+str,ans,number,number,target);
+                helper(j+1,n,num,target,digit,digit,s+temp,ans);
             }
             else{
-                helper(j+1,n,num,s+"+"+str,ans,cursum+number,number,target);
-                helper(j+1,n,num,s+"-"+str,ans,cursum-number,-number,target);
-                helper(j+1,n,num,s+"*"+str,ans,cursum-prevnum+prevnum*number,prevnum*number,target);
+                helper(j+1,n,num,target,cur+digit,digit,s+"+"+temp,ans);
+                helper(j+1,n,num,target,cur-digit,-digit,s+"-"+temp,ans);
+                helper(j+1,n,num,target,cur-prev+prev*digit,prev*digit,s+"*"+temp,ans);
             }
         }
     }
 public:
     vector<string> addOperators(string num, int target) {
-        vector<string> ans;
         int n=num.size();
-        helper(0,n,num,"",ans,0,0,target);
+        vector<string> ans;
+        
+        helper(0,n,num,target,0,0,"",ans);
         return ans;
     }
 };
