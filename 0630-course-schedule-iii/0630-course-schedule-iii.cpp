e4204990e184d00d14a7105ef097fee0e66a9cf2
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-08-03 08:57:28 +0530

    Time: 159 ms (99.65%), Space: 56.1 MB (95.67%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        
+        auto compare=[](const vector<int> &a,vector<int> &b){
+            return a[1]<b[1];
+        };
+        sort(courses.begin(),courses.end(),compare);
+        
+        int sum=0;
+        priority_queue<int> pq;
+        for(auto &it:courses){
+            sum+=it[0];
+            pq.push(it[0]);
+            
+            if(sum>it[1]){
+                sum-=pq.top();
+                pq.pop();
+            }
+        }
+        
+        return pq.size();
+    }
+};
