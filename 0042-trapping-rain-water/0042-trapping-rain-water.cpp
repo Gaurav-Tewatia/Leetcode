435d2a8e00e14dcddbd46910fb41108cd4fccbd9
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-08-03 15:29:40 +0530

    Time: 17 ms (55.90%), Space: 19.7 MB (90.69%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        // we can do it using extra 3N space 
+        
+        // but interviewer might gives a follow up question which asks to do it in O(1) space
+        
+        int maxleft=0,maxright=0;
         int n=height.size();
-        vector<int> left(n,0),right(n,0),width(n,0);
-        
-        left[0]=height[0];
-        for(int i=1;i<height.size();i++){
-            if(height[i]>left[i-1])
-                left[i]=height[i];
-            else
-                left[i]=left[i-1];
+        int left=0,right=n-1;
+        int res=0;
+        while(left<=right){
+            if(height[left]<=height[right]){
+                if(height[left]>=maxleft) maxleft=height[left];
+                else res+=maxleft-height[left];
+                left++;
+            }
+            else{
+                if(height[right]>=maxright) maxright=height[right];
+                else res+=maxright-height[right];
+                right--;
+            }
         }
-        
-        right[height.size()-1]=height[height.size()-1];
-        for(int i=height.size()-2;i>=0;i--){
-            if(height[i]>right[i+1])
-                right[i]=height[i];
-            else
-                right[i]=right[i+1];
-        }
-        
-        for(int i=0;i<n;i++){
-            width[i]=min(left[i],right[i]);
-        }
-        
-        int ans=0;
-        for(int i=0;i<n;i++){
-            ans+=width[i]-height[i];
-        }
-        
-        return ans;
+        return res;
     }
 };
