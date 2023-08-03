ab26657cb5849d2f170286f1c0daca081273fdb8
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-08-03 08:58:48 +0530

    Time: 201 ms (96.72%), Space: 56.1 MB (77.31%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.cpp
+++ 0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -1,10 +1,13 @@
 class Solution {
+   static  bool compare(const vector<int> &a, const vector<int> &b){
+        return a[1]<b[1];
+    }
 public:
     int scheduleCourse(vector<vector<int>>& courses) {
         
-        auto compare=[](const vector<int> &a,vector<int> &b){
-            return a[1]<b[1];
-        };
+        // auto compare=[](const vector<int> &a,vector<int> &b){
+        //     return a[1]<b[1];
+        // };
         sort(courses.begin(),courses.end(),compare);
         
         int sum=0;
