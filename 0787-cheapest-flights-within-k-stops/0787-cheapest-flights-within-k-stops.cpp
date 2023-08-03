cb5673a0ec450c3382eb8e25ef920d80349bf8ec
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-08-03 14:46:45 +0530

    Time: 22 ms (81.30%), Space: 13.8 MB (27.22%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-       vector<pair<int,int>> adj[n];
- 
-        for(auto c:flights){
-            adj[c[0]].push_back({c[1],c[2]});
-        }
-               priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        vector<int> dis(n,1e9);
+         typedef pair<int,pair<int,int>> pi;
+    vector<pair<int,int>> adj[n];
+    for(auto it:flights){
+        int u=it[0];
+        int v=it[1];
+        int wt=it[2];
+        adj[u].push_back({v,wt});
+    }
+
+    priority_queue<pi,vector<pi>, greater<pi>> pq;
+    pq.push({0,{0,src}});
+    vector<int> dis(n,1e9);
         dis[src]=0;
-        
-        pq.push({0,{src,0}}); //dis,node,kvalue
-        while(!pq.empty()){
-            int kvalue=pq.top().first;
-            int node=pq.top().second.first;
-            int dist=pq.top().second.second;
-            
-            pq.pop();
-            
-            if(kvalue>k)
-                continue;
-            
-            for(auto c:adj[node]){
-                int adjnode=c.first;
-                int wt=c.second;
-                if(dist+wt<dis[adjnode] and kvalue<=k){
-                    dis[adjnode]=dist+wt;
-                    pq.push({kvalue+1,{adjnode,dis[adjnode]}});
-                    
-                }
+    while(!pq.empty()){
+        int steps=pq.top().first;
+        int dist=pq.top().second.first;
+        int node=pq.top().second.second;
+
+        pq.pop();
+        if(steps>k) continue;
+
+        for(auto it:adj[node]){
+            int adjnode=it.first;
+            int wt=it.second;
+            if(steps<=k and dist+wt<dis[adjnode]){
+                dis[adjnode]=dist+wt;
+                pq.push({steps+1,{dis[adjnode],adjnode}});
             }
         }
-         if (dis[dst] == 1e9)
-            return -1;
-        return dis[dst];
+    }
+    if(dis[dst]==1e9) return -1;
+    return dis[dst];
     }
 };
