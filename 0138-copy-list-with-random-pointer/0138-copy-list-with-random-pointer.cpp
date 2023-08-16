b9aceeef6909073021c99dd471884dea56308181
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-08-16 21:05:56 +0530

    Time: 11 ms (39.21%), Space: 11.3 MB (57.96%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,21 +17,61 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*,Node*> mp;
+        //THIS WILL TAKE EXTRA O(N) space complexity
+//         unordered_map<Node*,Node*> mp;
+//         Node*temp=head;
+//         while(temp!=NULL){
+//             Node*n=new Node(temp->val);
+//             mp[temp]=n;
+//             temp=temp->next;
+//         }
+        
+//         temp=head;
+//         while(temp!=NULL){
+//             Node*n=mp[temp];
+//             n->next=temp->next!=NULL?mp[temp->next]:NULL;
+//             n->random=temp->random!=NULL?mp[temp->random]:NULL;
+//             temp=temp->next;
+//         }
+//         return mp[head];
+        
+        //THIS WILL TAKE O(1) space complexity
+        
+        
+        //create a deep copy of node
         Node*temp=head;
         while(temp!=NULL){
             Node*n=new Node(temp->val);
-            mp[temp]=n;
-            temp=temp->next;
+            n->next=temp->next;
+            temp->next=n;
+            temp=n->next;   // or temp->next->next
+            
         }
+        
+        
+        //link the random pointer of deep copied node
         
         temp=head;
         while(temp!=NULL){
-            Node*n=mp[temp];
-            n->next=temp->next!=NULL?mp[temp->next]:NULL;
-            n->random=temp->random!=NULL?mp[temp->random]:NULL;
-            temp=temp->next;
+            if(temp->random)
+            temp->next->random=temp->random->next;
+            temp=temp->next->next;
         }
-        return mp[head];
+        
+        //now link the next pointer of deep copied nodes and restore the original node as well
+        
+        Node*dummy=new Node(0);
+        Node*res=dummy;
+        temp=head;
+        while(temp!=NULL){
+            dummy->next=temp->next;
+            temp->next=temp->next->next;
+                
+            temp=temp->next;
+            dummy=dummy->next;
+        }
+        
+        return  res->next;
+        
     }
 };
