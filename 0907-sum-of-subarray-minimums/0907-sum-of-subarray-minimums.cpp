fa153de0d41bf368ba6009df98772bd6d1d1e500
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-08-20 09:09:08 +0530

    Time: 87 ms (69.54%), Space: 49.4 MB (5.62%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,67 +1,48 @@
 class Solution {
 public:
-    int modulo=1e9+7;
+    
+  // const int modulo=1e9+7;
     int sumSubarrayMins(vector<int>& arr) {
-        vector<long long> nsl; // for distance between previous smaller element and current
-        int n = arr.size();
-        vector<int> nsr;  //for distance between next smaller eleement to right and current
-        stack<int> st;
-        for(int i=0;i<arr.size();i++){
-            if(st.empty())
-                nsl.push_back(i+1); //pushing the ditstance between nsl and current element
-            else if (!st.empty() and arr[st.top()]<arr[i])
-                nsl.push_back(i-st.top());
-            else if(!st.empty() and arr[st.top()]>=arr[i]){
-                
-                while(!st.empty() and arr[st.top()]>=arr[i]){
-                    st.pop();
-                }
-                if(st.empty())
-                    nsl.push_back(i+1);
-                else
-                    nsl.push_back(i-st.top());
-            }
-            st.push(i);
-        }
-        
-        stack<int> temp;
-        st.swap(temp);
-        
-           for(int i=arr.size()-1;i>=0;i--){
-            if(st.empty())
-                nsr.push_back(n-i);
-            else if (!st.empty() and arr[st.top()]<arr[i])
-                nsr.push_back(st.top()-i);
-            else if(!st.empty() and arr[st.top()]>=arr[i]){
-                
-                while(!st.empty() and arr[st.top()]>arr[i]){
-                    st.pop();
-                }
-                if(st.empty())
-                    nsr.push_back(n-i);
-                else
-                    nsr.push_back(st.top()-i);
-            }
-            st.push(i);
-        }
-        
-        reverse(begin(nsr),end(nsr));
-        
-        for(auto c:nsl)
-            cout<<c<<" ";
-        cout<<endl;
-        for(auto c:nsr)
-            cout<<c<<" ";
+    const int modulo=1e9+7;
+    
+    int n=arr.size();
+    vector<long long> nsl;
+    vector<int> nsr;
 
-        
-
-        int count=0;
-        for(int i=0;i<arr.size();i++){
-              count+=(arr[i]*(nsr[i]*nsl[i])%modulo)%modulo;
- count%=modulo;
+    stack<int> st;
+    
+    for(int i=0;i<n;i++){
+        if(st.empty()) nsl.push_back(i+1);
+        else if(!st.empty() and arr[st.top()]<arr[i] ) nsl.push_back(i-st.top());
+        else if(!st.empty() and arr[st.top()]>=arr[i]){
+            while(!st.empty() and arr[st.top()]>=arr[i]) st.pop();
+            if(st.empty()) nsl.push_back(i+1);
+            else nsl.push_back(i-st.top());
         }
-        
-        return count;
+        st.push(i);
+    }
+
+        while(!st.empty()) st.pop();
+
+    for(int i=n-1;i>=0;i--){
+        if(st.empty()) nsr.push_back(n-i);
+        else if(!st.empty() and arr[st.top()]<arr[i]) nsr.push_back(st.top()-i);
+        else if(!st.empty() and arr[st.top()]>=arr[i]){
+            while(!st.empty() and arr[st.top()]>arr[i]) st.pop();
+            if(st.empty()) nsr.push_back(n-i);
+            else nsr.push_back(st.top()-i);
+        }
+
+        st.push(i);
     }
     
+        reverse(nsr.begin(),nsr.end());
+        int ans=0;
+    for(int i=0;i<n;i++){
+        ans+=(arr[i]*(nsl[i]*nsr[i])%modulo)%modulo;
+        ans=ans%modulo;
+    }
+
+    return  ans;
+    }
 };
