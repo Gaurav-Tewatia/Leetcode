4d9dff128dd12ed27ca0fb7dfc8cf06f90cbebf6
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-08-20 17:45:40 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -13,29 +13,45 @@ class Solution
     //Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& M, int n) 
     {
-        vector<int> adj[n+1];
+        // vector<int> adj[n+1];
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==j) continue;
+        //         if(M[i][j]==1) adj[i].push_back(j);
+        //     }
+        // }
+        // vector<int> ::iterator it;
+        // for(int i=0;i<n;i++){
+        //     if(adj[i].empty()){
+        //         bool flag=1;
+        //         for(int j=0;j<n;j++){
+        //             if(j!=i){
+        //                 it=find(adj[j].begin(),adj[j].end(),i);
+        //                 if(it==adj[j].end()){
+        //                      flag=0;
+        //                      break;
+        //                 }
+                       
+        //             }
+        //         }
+        //         if(flag) return i;
+        //     }
+        // }
+        // return -1;
+    
+        
+        vector<int> indegree(n,0),outdegree(n,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(i==j) continue;
-                if(M[i][j]==1) adj[i].push_back(j);
+                if(M[i][j]){
+                    indegree[j]++;
+                    outdegree[i]++;
+                }
             }
         }
-        vector<int> ::iterator it;
+        
         for(int i=0;i<n;i++){
-            if(adj[i].empty()){
-                bool flag=1;
-                for(int j=0;j<n;j++){
-                    if(j!=i){
-                        it=find(adj[j].begin(),adj[j].end(),i);
-                        if(it==adj[j].end()){
-                             flag=0;
-                             break;
-                        }
-                       
-                    }
-                }
-                if(flag) return i;
-            }
+            if(indegree[i]==n-1 and outdegree[i]==0) return i;
         }
         return -1;
     }
