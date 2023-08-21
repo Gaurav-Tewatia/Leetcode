34db33c2d9f77d798bd5c108c8c134f9b01d3b5d
Gaurav-Tewatia <103888220+Gaurav-Tewatia@users.noreply.github.com>
2023-08-21 13:16:14 +0530

    Time: 424 ms (87.70%), Space: 178.5 MB (29.52%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -3,40 +3,39 @@ public:
     class node{
         public:
         int key,value;
-        node*next;
-        node*prev;
+        node*next,*prev;
         node(int key,int value){
             this->key=key;
             this->value=value;
         }
     };
-    
     node*head=new node(-1,-1);
     node*tail=new node(-1,-1);
-    unordered_map<int,node*> mp;
     int size;
+    
     void addnode(node*n){
-        node*temp=head->next;
-        n->next=temp;
-        temp->prev=n;
+        n->next=head->next;
         n->prev=head;
+        head->next->prev=n;
         head->next=n;
-        
     }
-   void deletenode(node*n){
+    
+    void deletenode(node*n){
         node*pre=n->prev;
         node*nex=n->next;
         pre->next=nex;
         nex->prev=pre;
     }
+    
+    unordered_map<int,node*> mp;
     LRUCache(int capacity) {
         size=capacity;
         head->next=tail;
-        tail->prev=head;
+            tail->prev=head;
     }
     
     int get(int key) {
-        if(mp.find(key)!=mp.end()){
+        if(mp.count(key)){
             node*temp=mp[key];
             int res=temp->value;
             deletenode(temp);
@@ -45,25 +44,22 @@ public:
             mp[key]=head->next;
             return res;
         }
-        else
-             return -1;
+        else return -1;
     }
     
     void put(int key, int value) {
-        if(mp.find(key)!=mp.end()){
+        if(mp.count(key)){
             node*temp=mp[key];
-            mp.erase(key);
             deletenode(temp);
-            
+            mp.erase(key);
         }
         else if(mp.size()==size){
             mp.erase(tail->prev->key);
             deletenode(tail->prev);
         }
         
-            addnode(new node(key,value));
-            mp[key]=head->next;
-        
+        addnode(new node(key,value));
+        mp[key]=head->next;
     }
 };
 
